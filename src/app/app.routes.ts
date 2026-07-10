dea9ad4dbeddf7cdf202c2aebbaae5cb3aeafb54
Bakary COULIBALY <coulbybakary54@gmail.com>
2026-07-10 23:04:55 +0000

    Création model et service de avis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AvisTest } from './components/Avis-Test/avis-test/avis-test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'avis', component: AvisTest },
+  { path: '', redirectTo: 'avis', pathMatch: 'full' }
+];
