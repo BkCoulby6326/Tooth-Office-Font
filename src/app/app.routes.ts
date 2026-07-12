e36640a39dea4c6b5d68ce2b6f953473d6b8a5bf
Morbin <supermorba@gmail.com>
2026-07-12 13:09:39 +0000

    gestion d'erreurs mineurs pour lancer le projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { AvisTest } from './components/Avis-Test/avis-test/avis-test';
 
 export const routes: Routes = [
-  { path: 'avis', component: AvisTest },
+
   { path: '', redirectTo: 'avis', pathMatch: 'full' }
 ];
 
