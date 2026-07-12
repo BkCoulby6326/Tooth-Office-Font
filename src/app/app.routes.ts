c5b2bb823b7f04accde4551f3a5f771ded6a3188
ismaila <ismailasangare1604@gmail.com>
2026-07-12 09:37:34 +0000

    l'interface de la secretaire presque fini

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+
 import { AvisTest } from './components/Avis-Test/avis-test/avis-test';
 
 export const routes: Routes = [
   { path: 'avis', component: AvisTest },
   { path: '', redirectTo: 'avis', pathMatch: 'full' }
 ];
+
+
