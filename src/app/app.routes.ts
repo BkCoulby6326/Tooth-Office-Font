0f01197052e9d14329b9a1cb47d3d0c283e921c5
morbin <supermorba@gmail.com>
2026-07-13 07:39:04 +0200

    mise en place des bases pour l'auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
-
-  { path: '', redirectTo: 'avis', pathMatch: 'full' }
+  { path: 'login', loadComponent: () => import('./pages/auth/login/login.component').then((m) => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./pages/auth/register/register.component').then((m) => m.RegisterComponent) },
+  { path: 'accueil', loadComponent: () => import('./pages/auth/dashboard/dashboard.component').then((m) => m.DashboardComponent) },
+  { path: 'dashboard', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' }
 ];
 
 
